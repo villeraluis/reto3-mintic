0d2557e256922bb12ec3011862321d963c129fa2
unknown <villeraluis47@gmail.com>
2022-10-03 20:46:51 -0500

    config

---
+++ lib/config/configuration.dart
@@ -0,0 +1,14 @@
+//create a file called configuration.dart on this folder
+// copy the values from firebase
+
+class Configuration {
+  static const apiKey = "AIzaSyCnGyyZGSHGWchX93DWCI49Na7epV9Zzn0";  // tomado de current_key
+  static const authDomain = "1015857339914-nskbtog12hkm8c5mjsp0ncgt4carun10.apps.googleusercontent.com"; // tomado de oauth_client
+  static const databaseURL =
+      "https://chatapp-9eecc-default-rtdb.firebaseio.com"; // tomado de firebase_url
+  static const projectId = "chatapp-9eecc"; // tomado de project_id
+  static const storageBucket = "chatapp-9eecc.appspot.com"; // tomado de storage_bucket
+  static const messagingSenderId = "1015857339914"; //no lo vamos a usar
+  static const appId = "1:1015857339914:android:cfd408d9b5c80b3b9b49f3"; // tomado de mobilesdk_app_id
+  static const measurementId = ""; //no lo vamos a usar
+}
